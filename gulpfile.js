2b02fef41828b2bea8b332bb464be1fe097f1d3d
Facundo Zaldo <fzaldo@gmail.com>
2016-06-14 10:29:11 -0300

    Instalacion de gulp

--- gulpfile.js
+++ gulpfile.js
@@ -12,5 +12,25 @@ var elixir = require('laravel-elixir');
  */
 
 elixir(function(mix) {
-    mix.sass('app.scss');
+	mix.sass('app.scss');
+
+	
+	mix.styles([
+			"./node_modules/admin-lte/dist/css/AdminLTE.min.css",
+			"./node_modules/admin-lte/dist/css/skins/skin-blue.min.css"
+		], 'public/css/libs.css');
+	
+
+	mix.browserSync({
+		files: [
+			   'app/**/*',
+			   'public/**/*',
+			   'resources/views/**/*'
+		   ],
+		   proxy: 'localhost/spcms/public/',
+		   browser: '/opt/firefox_dev/firefox',
+		   ws: true,
+	});
+
+
 });
--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5</div>
+                <div class="title">Pagina Principal</div>
             </div>
         </div>
     </body>
